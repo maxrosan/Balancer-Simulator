425be7da2242fb72da165226704f25ad26d38954
Max Rosan <max.rosan@ymail.com>
2013-02-01 02:49:11 -0200

    Toyoda method

---
+++ methods/toyodamethod.py
@@ -0,0 +1,104 @@
+
+
+import loadbalacing, random, usageclass
+import numpy
+import math
+
+# RandomMethod selects a task randomly to run
+#
+class ToyodaMethod(loadbalacing.LoadBalacing):
+
+	def __init__(self):
+		loadbalacing.LoadBalacing.__init__(self)
+	
+
+	def run(self, tasks, mac):
+		
+		n = len(tasks)
+
+		Tu = []
+		Td = range(0, n)
+		Pu = numpy.zeros(2, int)
+		Z  = 0
+		X  = numpy.zeros(n, int)
+		Tc = []
+		B  = numpy.ones(2, float)
+
+		P  = numpy.zeros((n, 2), float)
+		G  = numpy.zeros(n, float)
+
+		U  = numpy.zeros(n, float)
+
+		for x in range(0, n):
+			P[x][0] = tasks[x].CPU_usage / mac.capacity_CPU
+			P[x][1] = tasks[x].mem_usage / mac.capacity_memory
+
+		keep_going = True
+
+		cnt = math.sqrt(2)
+
+		while keep_going :
+
+			print "%d, %d" % (len(Tu), len(Td))
+	
+			# step 2
+			del Tc
+			Tc = []
+			
+			for i in Td:
+				if P[i][0] <= (1 - Pu[0]) and P[i][1] <= (1 - Pu[1]):
+					Tc.append(i)
+
+			print "2"
+
+			# step 3
+			# terminate if Tc = empty
+			if len(Tc) == 0:
+				keep_going = False
+			else:
+
+				# step 4
+				# (a)
+				if (numpy.dot(Pu, Pu) == 0.):
+					for i in Tc:
+						d    = sum(P[i])
+						G[i] = (tasks[i].CPU_usage * cnt)/d
+				# (b)
+				else:
+					mod_Pu = math.sqrt(numpy.dot(Pu, Pu))
+					E      = numpy.array(Pu * (1./mod_Pu))
+				
+					for i in Tc:
+						d    = numpy.dot(P[i], E)
+						G[i] = tasks[i].CPU_usage / d
+
+				print "4"
+
+				# step 5
+				v_max = -1
+				i_max = 0
+				for i in Tc:
+					if G[i] > v_max:
+						v_max = G[i]
+						i_max = i
+
+
+				print "5"
+				# step 6
+				Tu.append(i_max)
+				Td.remove(i_max)
+				Pu = Pu + P[i_max]
+				Z  = Z + tasks[i_max].CPU_usage
+
+
+		print Tu
+		
+
+	def balance(self, machines_ready, tasks_to_run, tasks_constraints): 
+		
+		self.n_round = self.n_round + 1
+		self.reset_stats()
+
+		for mac in machines_ready:
+			self.run(list(tasks_to_run), mac)	
+			break
