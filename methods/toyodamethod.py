e3dde2ff3cde609ec19e660f1d160db0ef03b53e
Max Rosan <max.rosan@ymail.com>
2013-02-21 18:32:21 -0300

    Refact

--- methods/toyodamethod.py
+++ methods/toyodamethod.py
@@ -244,9 +244,8 @@ class ToyodaMethod(loadbalacing.LoadBalacing):
 		def update_map(macs):
 			for mac_ID in macs:
 				for task in macs[mac_ID]:
-					if self.machines_state[mac_ID].can_run(self.tasks_state[task]):
-						self.machines_state[mac_ID].add_task(self.tasks_state, task)
-						self.tasks_state[task].machine_ID = mac_ID
+					self.machines_state[mac_ID].add_task(self.tasks_state, task)
+					self.tasks_state[task].machine_ID = mac_ID
 	
 		self.n_threads  = self.n_jobs
 	
@@ -307,10 +306,9 @@ class ToyodaMethod(loadbalacing.LoadBalacing):
 			macs = ToyodaMethod.balance_partial(None, self.machines_state, self.tasks_state, macs_not_used, tasks_without_mac)
 			for mac_ID in macs:
 				for task in macs[mac_ID]:
-					if self.machines_state[mac_ID].can_run(self.tasks_state[task]):
-						self.machines_state[mac_ID].add_task(self.tasks_state, task)
-						self.tasks_state[task].machine_ID = mac_ID
-						tasks_without_mac.remove(task)
+					self.machines_state[mac_ID].add_task(self.tasks_state, task)
+					self.tasks_state[task].machine_ID = mac_ID
+					tasks_without_mac.remove(task)
 
 
 #		SLAs_list = []
@@ -334,13 +332,13 @@ class ToyodaMethod(loadbalacing.LoadBalacing):
 		(self.machines_used, self.machines_not_used)             = self.__count_macs()
 
 
-		if self.SLA_breaks > 0:
-			for mac_ID in self.machines_state:
-				mac = self.machines_state[mac_ID]
-				if mac.SLA_break():
-					print "%d (%f, %f, %f, %f) :" % (mac.machine_ID, mac.capacity_CPU, mac.capacity_memory, mac.CPU_usage, mac.mem_usage),
-					for task_ID in mac.tasks:
-						task = self.tasks_state[task_ID]
-						print "%d (%f, %f), " % (task_ID, task.CPU_usage, task.mem_usage),
-					print "----"
-			exit()
+		#if self.SLA_breaks > 0:
+		#	for mac_ID in self.machines_state:
+		#		mac = self.machines_state[mac_ID]
+		#		if mac.SLA_break():
+		#			print "%d (%f, %f, %f, %f) :" % (mac.machine_ID, mac.capacity_CPU, mac.capacity_memory, mac.CPU_usage, mac.mem_usage),
+		#			for task_ID in mac.tasks:
+		#				task = self.tasks_state[task_ID]
+		#				print "%d (%f, %f), " % (task_ID, task.CPU_usage, task.mem_usage),
+		#			print "----"
+			#exit()
