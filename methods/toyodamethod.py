d34e3e4d85aecd42fae91fff54b5830ce95faf40
Max Rosan <max.rosan@ymail.com>
2013-02-21 17:29:36 -0300

    Refact

--- methods/toyodamethod.py
+++ methods/toyodamethod.py
@@ -244,7 +244,7 @@ class ToyodaMethod(loadbalacing.LoadBalacing):
 		def update_map(macs):
 			for mac_ID in macs:
 				for task in macs[mac_ID]:
-					if self.machines_state[mac_ID].can_run(task):
+					if self.machines_state[mac_ID].can_run(self.tasks_state[task]):
 						self.machines_state[mac_ID].add_task(self.tasks_state, task)
 						self.tasks_state[task].machine_ID = mac_ID
 	
@@ -307,7 +307,7 @@ class ToyodaMethod(loadbalacing.LoadBalacing):
 			macs = ToyodaMethod.balance_partial(None, self.machines_state, self.tasks_state, macs_not_used, tasks_without_mac)
 			for mac_ID in macs:
 				for task in macs[mac_ID]:
-					if self.machines_state[mac_ID].can_run(task):
+					if self.machines_state[mac_ID].can_run(self.tasks_state[task]):
 						self.machines_state[mac_ID].add_task(self.tasks_state, task)
 						self.tasks_state[task].machine_ID = mac_ID
 						tasks_without_mac.remove(task)
