bcad885627291278ecdbc9c35a1007d55117ca65
Max Rosan <max.rosan@ymail.com>
2013-03-01 18:16:12 -0300

    Get information about tasks

--- config_tasks_log.py
+++ config_tasks_log.py
@@ -2,8 +2,6 @@
 method              = "stats"
 num_of_jobs         = 0
 dataset_path        = "/home/maxrosan/src/gs_cluster/"
-mapping_log         = "mapping_stats.log"
-balancing_log       = "balancing_stats.log"
 total_time          = 10000
 logfile             = "stats.log"
 mapping_log         = "/dev/null"
