af0fcfb8fb8b80fbaabaf4c7d905b690050249aa
Max Rosan <max.rosan@ymail.com>
2013-03-05 15:54:21 -0300

    New intervals to run

--- sched_brucutu.sh
+++ sched_brucutu.sh
@@ -2,7 +2,7 @@
 
 #configs=("config_vars_04.py" "config_vars_06.py" "config_vars_08.py" "config_vars_1.py")
 
-configs=("config_interval_05.py" "config_interval_10.py" "config_interval_20.py" "config_interval_40.py" "config_interval_80.py")
+configs=("config_vars_04_brucutu.py")
 
 for cfg in ${configs[*]}; do
 	~/brucutu/pypy-c-jit-59498-b03871503962-linux/bin/pypy ./main.py config/$cfg;
