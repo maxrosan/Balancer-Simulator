0e03d38d314199e0273ecb821f1582eb33ca4f25
Max Rosan <max.rosan@ymail.com>
2013-03-05 18:50:10 -0300

    Checking if all values read from the trace are valid values

--- config/config_tasks_log_brucutu.py
+++ config/config_tasks_log_brucutu.py
@@ -3,6 +3,6 @@ method              = "stats"
 num_of_jobs         = 0
 dataset_path        = "/var/tmp/mr/gs_cluster/"
 total_time          = (60 * 60 * 24) * 1
-logfile             = "log/stats.log"
+logfile             = "/var/tmp/mr/log/stats.log"
 mapping_log         = "/dev/null"
 balancing_log       = "/dev/null"
