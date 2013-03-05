012ff56476fa743a7863f368c6c8fca288effc9e
Max Rosan <max.rosan@ymail.com>
2013-03-05 09:29:44 -0300

    Time interval changed

--- config/config_tasks_log.py
+++ config/config_tasks_log.py
@@ -3,6 +3,6 @@ method              = "stats"
 num_of_jobs         = 0
 dataset_path        = "/home/maxrosan/src/gs_cluster/"
 total_time          = 10000
-logfile             = "stats.log"
+logfile             = "log/stats.log"
 mapping_log         = "/dev/null"
 balancing_log       = "/dev/null"
