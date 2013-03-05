38da3a5faf3c9d1dd6154401486cbad61bb9497f
Max Rosan <max.rosan@ymail.com>
2013-03-05 16:10:22 -0300

    New intervals to run

---
+++ config/config_tasks_log_brucutu.py
@@ -0,0 +1,8 @@
+
+method              = "stats"
+num_of_jobs         = 0
+dataset_path        = "/var/tmp/mr/gs_cluster/"
+total_time          = (60 * 60 * 24) * 2
+logfile             = "log/stats.log"
+mapping_log         = "/dev/null"
+balancing_log       = "/dev/null"
---
+++ config/config_vars_06_brucutu.py
@@ -0,0 +1,9 @@
+method              = 'toyoda'
+num_of_jobs         = 8
+interval_toyoda     = 10
+dataset_path        = "/var/tmp/mr/gs_cluster/"
+mapping_log         = "log/mapping_toyoda_06_brucutu.log"
+balancing_log       = "log/balancing_toyoda_06_brucutu.log"
+w_cpu               = 0.6
+w_mem               = 1 - w_cpu
+total_time          = (60 * 60 * 24) * 2
