61775fa30939f3666aae4d5245efc5f2817f2eba
Max Rosan <max.rosan@ymail.com>
2013-03-05 10:22:08 -0300

    New intervals to run

---
+++ config/config_interval_80.py
@@ -0,0 +1,11 @@
+
+
+method              = 'toyoda'
+num_of_jobs         = 8
+interval_toyoda     = 80
+dataset_path        = "/home/maxrosan/src/gs_cluster/"
+mapping_log         = "log/mapping_toyoda_04.log"
+balancing_log       = "log/balancing_toyoda_04.log"
+w_cpu               = 0.4
+w_mem               = 1 - w_cpu
+total_time          = (60 * 60 * 24) * 2
