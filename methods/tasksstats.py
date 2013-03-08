e07db5d82f37ddd1c3ac1fbb7e067602e5f900c6
Max Rosan <max.rosan@ymail.com>
2013-03-08 11:08:00 -0300

    Tasks stats

--- methods/tasksstats.py
+++ methods/tasksstats.py
@@ -121,8 +121,8 @@ class TasksStats(loadbalacing.LoadBalacing):
 				
 			self.tasks[task.getID()].CPU_usage = cpu
 			self.tasks[task.getID()].mem_usage = mem
-			
-			self.hist[task.getID()].append((cpu, mem))
+			if task_obj.last_round < self.n_round + 1:
+				self.hist[task.getID()].append((cpu, mem))
 		else:
 			self.tasks[task.getID()] = task
 			self.hist[task.getID()]  = [(task.CPU_usage, task.mem_usage)]
