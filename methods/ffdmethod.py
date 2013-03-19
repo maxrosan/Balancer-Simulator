92e9664f163963505b787c7b90ba29efd54a6f84
Max <max.rosan@ymail.com>
2013-03-19 17:29:42 -0300

    Bin packing implemented

---
+++ methods/ffdmethod.py
@@ -0,0 +1,43 @@
+
+import binpackingmethod
+import numpypy as numpy
+import math, random, Queue
+
+import threading, multiprocessing, time, sys
+
+## FFD without prediction
+class FFDMethod(binpackingmethod.BinPackingMethod):
+
+	def __init__(self, threshold, w_cpu, w_mem):
+		binpackingmethod.BinPackingMethod.__init__(self, threshold)
+
+		self.w_cpu = w_cpu
+		self.w_mem = w_mem
+
+	def bin_packing(self, tasks, macs):
+
+		def score_task(task):
+			dcpu = self.w_cpu * task.CPU_usage
+			dmem = self.w_mem * task.mem_usage
+
+			return (dcpu + dmem)
+
+		def score_mac(mac):
+			return mac.free_CPU()
+
+		tasks_sorted = sorted(tasks, key=lambda task_ID:score_task(self.tasks_state[task_ID]), reverse=True)
+		macs_sorted  = sorted([mac for mac in macs if self.machines_state[mac].count_tasks() > 0], 
+		 key=lambda mac_ID:score_mac(self.machines_state[mac_ID]), reverse=True) + \
+		 sorted([mac for mac in macs if self.machines_state[mac].count_tasks() == 0],
+		  key=lambda mac_ID:score_mac(self.machines_state[mac_ID]), reverse=True)
+
+		for task in tasks_sorted:
+			for mac in macs_sorted:
+				if self.machines_state[mac].can_run(self.tasks_state[task]):
+
+					obj            = self.tasks_state[task]
+					obj.machine_ID = mac
+
+					self.machines_state[mac].add_task(obj)
+
+					break
