0eb787d07379581ce561751d63230db1febd873a
Max <max.rosan@ymail.com>
2013-03-25 17:22:52 -0300

    Mac list

--- config/toyoda/config_weight_04.py
+++ config/toyoda/config_weight_04.py
@@ -7,7 +7,7 @@ dataset_path        = "/var/tmp/mr/gs_cluster/"
 mapping_log         = "/var/tmp/mr/log/mapping_toyoda_04_brucutu.log"
 balancing_log       = "/var/tmp/mr/log/balancing_toyoda_04_brucutu.log"
 total_time          = (60 * 60 * 24) * 1
-method_sel_macs     = 'list'
+method_sel_macs     = 'priority_queue'
 
 def mac_key_sort(mac):
 	return mac.capacity_CPU * mac.capacity_memory
