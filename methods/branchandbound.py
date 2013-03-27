820324da23b00d5acaa68e9ddfbe202218eed296
Max Rosan <max.rosan@ymail.com>
2013-03-27 17:07:15 -0300

    Refactoring toyoda

--- methods/branchandbound.py
+++ methods/branchandbound.py
@@ -17,7 +17,7 @@ class BranchAndBoundMethod(binpackingmethod.BinPackingMethod):
 			return 0
 		elif i == N:
 			return M
-		elif (((N - i - 1) + M) * 0.7) > self.Gv:
+		elif (((N - i - 1) + M) * 0.4) > self.Gv:
 			x = self.__bbm(tasks, mac, i+1, M + tasks[i].CPU_usage*mac.free_CPU() + tasks[i].mem_usage*mac.free_mem(), N, value)
 			y = self.__bbm(tasks, mac, i+1, M, N, value)
 
