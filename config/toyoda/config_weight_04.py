9d1977a6e6ea7c438ebcad91fbb8b5df34908a9b
Max Rosan <max.rosan@ymail.com>
2013-03-28 16:18:42 -0300

    Refactoring toyoda

--- config/toyoda/config_weight_04.py
+++ config/toyoda/config_weight_04.py
@@ -11,13 +11,13 @@ method_sel_macs     = 'priority_queue'
 #method_sel_macs     = 'list'
 
 def mac_key_sort(mac):
-	return mac.capacity_CPU*mac.capacity_memory
+	return (mac.free_CPU(), mac.free_mem())
 
 def mac_key_pq(mac):
 	return mac.free_CPU()
 
 def task_key_sort(task):
-	return (1. + task.CPU_usage)*(1. + task.mem_usage)
+	return (task.CPU_usage, task.mem_usage)
 
 def score_task_knapsack(task, mac):
 	return (task.CPU_usage*mac.free_CPU()*0.4 +  task.mem_usage*mac.free_mem()*0.6)
