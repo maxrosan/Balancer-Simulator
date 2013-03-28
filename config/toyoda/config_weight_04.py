8b51afa5f49f8697ced0060720120c62cae0ae36
Max Rosan <max.rosan@ymail.com>
2013-03-28 16:05:21 -0300

    Refactoring toyoda

--- config/toyoda/config_weight_04.py
+++ config/toyoda/config_weight_04.py
@@ -7,7 +7,8 @@ dataset_path        = "/var/tmp/mr/gs_cluster/"
 mapping_log         = "/var/tmp/mr/log/mapping_toyoda_04_brucutu.log"
 balancing_log       = "/var/tmp/mr/log/balancing_toyoda_04_brucutu.log"
 total_time          = (60 * 60 * 24) * 1
-method_sel_macs     = 'priority_queue'
+#method_sel_macs     = 'priority_queue'
+method_sel_macs     = 'list'
 
 def mac_key_sort(mac):
 	return mac.capacity_CPU*mac.capacity_memory
@@ -19,7 +20,7 @@ def task_key_sort(task):
 	return (1. + task.CPU_usage)*(1. + task.mem_usage)
 
 def score_task_knapsack(task, mac):
-	return (task.CPU_usage*mac.free_CPU()*0.3 +  task.mem_usage*mac.free_mem()*0.7)
+	return (task.CPU_usage*mac.free_CPU()*0.4 +  task.mem_usage*mac.free_mem()*0.6)
 
 def must_migrate(old_task, new_task, machine):
 	#return ((1. + old_task.CPU_usage)*(1. + old_task.mem_usage) < (1. + new_task.CPU_usage)*(1. + new_task.mem_usage))
