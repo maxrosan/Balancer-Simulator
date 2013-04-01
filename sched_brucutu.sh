0387106d1621ee8e789235a0d45d1c98bce2f239
Max Rosan <max.rosan@ymail.com>
2013-04-01 14:45:06 -0300

    Sched updated

--- sched_brucutu.sh
+++ sched_brucutu.sh
@@ -4,7 +4,7 @@
 
 #configs=("config_tasks_log_brucutu.py" "config_vars_06_brucutu.py" "config_vars_1_brucutu.py" "config_vars_04_brucutu.py" "config_vars_08_brucutu.py")
 
-configs=("config_vars_04_brucutu.py" "config_vars_04_ffd_brucutu.py")
+configs=("config/toyoda/config_vars_04_brucutu.py" "config/ffd/config_vars_04_ffd_brucutu.py")
 
 for cfg in ${configs[*]}; do
 	~/brucutu/pypy-c-jit-59498-b03871503962-linux/bin/pypy ./main.py config/$cfg;
