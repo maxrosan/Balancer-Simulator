cfc23c2d9d395ae58ff4ab08215b82331da85854
Max <max.rosan@ymail.com>
2013-04-05 17:10:07 -0300

    Pag5

--- config/test/test.py
+++ config/test/test.py
@@ -2,14 +2,12 @@
 import site
 site.addsitedir("/var/tmp/mr/simulator/Balancer-Simulator/")
 
-from methods.loadbalancingalgorithm import LoadBalancingAlgorithm
-from prediction.NoPrediction import NoPrediction
-from migration_policy.MachineUsageMigration import MachineUsageMigration
+import methods.loadbalancingalgorithm, prediction.NoPrediction, migration_policy
 
-method              = LoadBalancingAlgorithm(NoPrediction(), MachineUsageMigration())
+method         = methods.loadbalancingalgorithm.LoadBalancingAlgorithm(prediction.NoPredictionNo.Prediction(), 
+ migration_policy.MachineUsageMigration.MachineUsageMigration())
 
-dataset_path        = "/var/tmp/mr/gs_cluster/"
-mapping_log         = "/var/tmp/mr/log/mapping_toyoda_04_brucutu.log"
-balancing_log       = "/var/tmp/mr/log/balancing_toyoda_04_brucutu.log"
-
-total_time          = (60 * 60 * 24) * 1
+dataset_path   = "/var/tmp/mr/gs_cluster/"
+mapping_log    = "/var/tmp/mr/log/mapping_toyoda_04_brucutu.log"
+balancing_log  = "/var/tmp/mr/log/balancing_toyoda_04_brucutu.log"
+total_time     = (60 * 60 * 24) * 1
