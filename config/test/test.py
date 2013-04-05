0756b734b5a0fd32f2a3af5e552b529450db3464
Max <max.rosan@ymail.com>
2013-04-05 15:58:00 -0300

    Pag5

---
+++ config/test/test.py
@@ -0,0 +1,12 @@
+
+import methods.loadbalancingalgorithm.LoadBalancingAlgorithm
+import prediction.NoPrediction.NoPrediction
+import migration_policy.MachineUsageMigration.MachineUsageMigration
+
+method              = LoadBalancingAlgorithm(NoPrediction(), MachineUsageMigration())
+
+dataset_path        = "/var/tmp/mr/gs_cluster/"
+mapping_log         = "/var/tmp/mr/log/mapping_toyoda_04_brucutu.log"
+balancing_log       = "/var/tmp/mr/log/balancing_toyoda_04_brucutu.log"
+
+total_time          = (60 * 60 * 24) * 1
