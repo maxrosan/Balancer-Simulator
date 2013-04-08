b95b3071a0416132f7acff91546976160192482f
Max <max.rosan@ymail.com>
2013-04-08 10:27:49 -0300

    Toyoda

--- config/test/test_knapsack.py
+++ config/test/test_knapsack.py
@@ -23,6 +23,12 @@ if host == "brucutu":
 	mapping_log   = "/var/tmp/mr/log/mapping_toyodaknapsack_test_brucutu.log"
 	balancing_log = "/var/tmp/mr/log/balancing_toyodaknapsack_test_brucutu.log"
 
+elif host == "godzilla":
+
+	dataset_path  = "/home/maxrosan/src/gs_cluster/"
+	mapping_log   = "/home/maxrosan/simulator/Balancer-Simulator/mapping_toyodaknapsack_test_brucutu.log"
+	balancing_log = "/home/maxrosan/simulator/Balancer-Simulator/balancing_toyodaknapsack_test_brucutu.log"	
+
 else:
 	dataset_path  = "/home/max/Src/simulator/gscluster/"
 	mapping_log   = "/home/max/Src/simulator/logop/mapping_toyodaknapsack_test_brucutu.log"
