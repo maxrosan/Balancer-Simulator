5f0c92c81cc90ee89c484e17d98ea478725d1b25
Max <max.rosan@ymail.com>
2013-04-08 10:28:53 -0300

    Toyoda

--- sched.sh
+++ sched.sh
@@ -2,9 +2,8 @@
 ##
 
 #configs=("config_vars_04.py" "config_vars_06.py" "config_vars_08.py" "config_vars_1.py")
+#configs=("config_interval_05.py" "config_interval_10.py" "config_interval_20.py" "config_interval_40.py" "config_interval_80.py")
 
-configs=("config_interval_05.py" "config_interval_10.py" "config_interval_20.py" "config_interval_40.py" "config_interval_80.py")
-
-for cfg in ${configs[*]}; do
-	~/simulator/pypy/pypy-c-jit-59498-b03871503962-linux/bin/pypy ./main.py config/$cfg;
-done;
+#for cfg in ${configs[*]}; do
+~/simulator/pypy/pypy-c-jit-59498-b03871503962-linux/bin/pypy ./main.py $1;
+#done;
