4b9d4a6aa46e06f1050fc8433ea8a67dfd8de3d5
Max <max.rosan@ymail.com>
2013-04-09 00:39:39 -0300

    Toyoda

--- sched.sh
+++ sched.sh
@@ -2,13 +2,13 @@
 ##
 
 #configs=("config_vars_04.py" "config_vars_06.py" "config_vars_08.py" "config_vars_1.py")
-configs=("config/test/test_knapsack.py", "config/test/test_ffd.py")
+configs=("config/test/test_knapsack.py" "config/test/test_ffd.py")
 
 for cfg in ${configs[*]}; do
 	HOST=`hostname`
 
 	if [ "x$HOST" == "xgodzilla" ]; then
-		~/simulator/pypy/pypy-c-jit-59498-b03871503962-linux/bin/pypy ./main.py $cfg;
+		~/simulator/pypy/pypy-c-jit-59498-b03871503962-linux/bin/pypy ./main.py $cfg ;
 	else
 		pypy ./main.py $1;
 	fi;
