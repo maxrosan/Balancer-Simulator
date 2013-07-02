cfe1447041100ba1d8ac881bd7db603decbea458
Max <max.rosan@ymail.com>
2013-07-02 14:55:38 -0300

    Average method

---
+++ config/test/test_knapsack_avg.py
@@ -0,0 +1,48 @@
+import site, commands
+import methods.toyodaknapsack, prediction.NoPrediction
+import migration_policy.MachineUsageMigration, migration_policy.SLABreakMigration
+
+def mac_key_sort(mac):
+	gain = (mac.free_CPU()*mac.free_CPU() + mac.free_mem()*mac.free_mem())
+	return gain
+
+def task_key_sort(task):
+	#gain = (task.CPU_usage*task.CPU_usage + task.mem_usage*task.mem_usage)
+	#pun  = 1./(1. + abs(task.CPU_usage - task.mem_usage))
+	return max(task.CPU_usage, task.mem_usage)
+
+def score_task_knapsack(task, mac):
+	return task.CPU_usage*task.CPU_usage + task.mem_usage*task.mem_usage
+
+migration_policies = [ migration.policy.AverageMigration.AverageMigration(5), migration_policy.SLABreakMigration.SLABreakMigration() ]
+
+method         = methods.toyodaknapsack.ToyodaKnapsack(prediction.NoPrediction.NoPrediction(), 
+ migration_policies, 8, score_task_knapsack,
+ mac_key_sort, task_key_sort)
+
+host = commands.getoutput("hostname")
+
+mapping_fname = "mapping_knapsack_test_avg.log"
+balancing_fname = "balancing_knapsack_test_avg.log"
+
+dataset_path = ""
+path_log     = ""
+
+if host == "brucutu":
+
+	dataset_path  = "/var/tmp/mr/gs_cluster/"
+	path_log   = "/var/tmp/mr/log/"
+
+elif host == "godzilla":
+
+	dataset_path  = "/home/maxrosan/src/gs_cluster/"
+	path_log   = "/home/maxrosan/simulator/Balancer-Simulator/log/"	
+
+else:
+	dataset_path  = "/home/max/Src/simulator/gscluster/"
+	path_log   = "/home/max/Src/simulator/logop/"
+
+mapping_log   = path_log + mapping_fname
+balancing_log = path_log + balancing_fname
+
+total_time     = (60 * 60 * 24) * 1
--- sched.sh
+++ sched.sh
@@ -2,7 +2,9 @@
 ##
 
 #configs=("config_vars_04.py" "config_vars_06.py" "config_vars_08.py" "config_vars_1.py")
-configs=("config/test/test_knapsack.py" "config/test/test_ffd.py")
+#configs=("config/test/test_knapsack.py" "config/test/test_ffd.py")
+
+configs=("config/test/test_knapsack_avg.py")
 
 for cfg in ${configs[*]}; do
 	HOST=`hostname`
