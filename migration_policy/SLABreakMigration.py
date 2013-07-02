99bab8c06641ce42cf2f03e7a655b99a8e38763e
Max <max.rosan@ymail.com>
2013-07-02 15:40:19 -0300

    Average mig

--- migration_policy/SLABreakMigration.py
+++ migration_policy/SLABreakMigration.py
@@ -4,6 +4,8 @@ class SLABreakMigration(migration_policy.Migration.Migration):
 
 	def __init__(self):
 		migration_policy.Migration.Migration()
+	
+		self.has_print_info = False
 
 	def must_migrate(self, old_task, new_task, machine):
 
